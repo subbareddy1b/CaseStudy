3ec3b5a2c2864bcbaff932ac41f15068af324a37
subbareddy1b <60545532+subbareddy1b@users.noreply.github.com>
2022-01-31 21:54:40 +0530

    Update SwapNos.java

--- JavaAssignment/SwapNos.java
+++ JavaAssignment/SwapNos.java
@@ -1,13 +1,42 @@
 package JavaAssignment;
 
-public class SwapNos {
-    public static void main(String[] args) {
-        int[] arr = {1,2,3};
-        int temp = arr[0];
-        arr[0] = arr[1];
-        arr[1] = temp;
-        for(int i=0; i<arr.length; i++){
-            System.out.println(arr[i]);
-        }
-    }
+import java.util.*;
+public class SwapNos{
+
+	public static void swap(int arr[]) {
+		int i=0;
+		int j=1;
+		int k=0;
+		int res[] = new int[arr.length];
+		while(i<arr.length&&j<arr.length) {
+		res[k] = arr[j];
+		k++;
+		res[j] =arr[i];
+		k++;
+		i = j+1;
+		j = i+1;
+		
+		}
+		
+		while(i<arr.length) {
+			res[i] = arr[i];
+			i++;
+		}
+		
+		for(int i1=0;i1<arr.length;i1++) {
+			System.out.print(res[i1]+" ");
+		}
+	}
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the Array Size : ");
+		int n = sc.nextInt();
+		int arr[] = new int[n];
+		for(int i=0;i<arr.length;i++) {
+			arr[i] = sc.nextInt();
+		}
+       swap(arr);
+	}
+
 }
